9e819de16c8de1efdf090b8c5f88c12a1e7efeef
Silmi Khashaish Fillah <silmi.k.fillah@gmail.com>
2023-09-02 22:26:28 +0700

    Add Test Suites for Feature Edit Cart - TS & TSC

---
+++ Test Suites/Feature Edit Item in Cart (from Mini Cart).ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Edit Item in Cart (from Mini Cart)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>43622139-d63e-4dc9-80c8-a81c965f76f8</testSuiteGuid>
+   <testCaseLink>
+      <guid>a241774f-7a7a-4422-b438-fb454bf71171</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Edit Item in Cart/UPCART01 - Edit item from Mini Cart with valid credentials</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2650350-66fb-459a-818a-e767176149ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Edit Item in Cart/UPCART03 - Edit quantity product from Mini Cart</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Feature Edit Item in Cart (from Shopping Cart Page).ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Edit Item in Cart (from Shopping Cart Page)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6e4749f0-e4d9-4766-9371-44c8b689517f</testSuiteGuid>
+   <testCaseLink>
+      <guid>f3066ee0-0dd3-4503-8f11-13ed1af9ab27</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Edit Item in Cart/UPCART02 - Edit item from Shopping Cart Page with valid credentials</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a35f4480-a18f-4444-b20c-6bd79cb792f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Edit Item in Cart/UPCART04 - Edit quantity product from Shopping Cart Page with valid input</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>742bc8f6-81c4-46e5-a515-450765a69a97</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Edit Item in Cart/UPCART05 - Edit item in cart with unselected size</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>01730c9d-6eff-4b4d-8840-028c0f77bce2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Edit Item in Cart/UPCART06 - Edit item in cart with unselected color</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04c36fc1-3f2d-4246-9697-5b5a9d17841b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Edit Item in Cart/UPCART07 - Edit item in cart with empty quantity</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
